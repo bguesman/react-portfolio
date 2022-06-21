0c454065f3fa5426a37272f10ec4570a55cbceea
Brad Guesman <james_guesman@alumni.brown.edu>
2022-06-21 14:11:22 -0700

    Fix mount function, try some stuff

--- app/src/three/fluid/Fluid.js
+++ app/src/three/fluid/Fluid.js
@@ -5,23 +5,35 @@ import * as THREE from "three";
 class ThreeFluid {
 
   // This object is constructed when its parent react component is loaded
-  constructor() {
+  constructor(mount) {
+    this.setupScene(mount);
+    this.registerListeners();
+    this.animate();
+  }
+
+  cleanup() {
+    this.deregisterListeners();
+    this.mount.removeChild(this.renderer.domElement);
+  }
+
+  setupScene(mount) {
+    this.mount = mount;
+    
     this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    this.camera = new THREE.PerspectiveCamera( 75, this.mount.clientWidth/this.mount.clientHeight, 0.1, 1000 );
+    this.camera.position.z = 5;
+    
     this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.renderer.setSize( this.mount.clientWidth, this.mount.clientHeight );
     this.renderer.setClearColor( 0xffffff, 1);
-    document.body.appendChild( this.renderer.domElement );
+    
+    this.mount.appendChild(this.renderer.domElement);
+
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
     var material = new THREE.MeshBasicMaterial( { color: 0xeb4034 } );
     // Keep a reference to the cube since we'll animate it.
     this.cube = new THREE.Mesh(geometry, material);
     this.scene.add(this.cube);
-    this.camera.position.z = 5;
-
-    this.registerListeners();
-
-    this.animate();
   }
   
   render() {
@@ -34,7 +46,6 @@ class ThreeFluid {
     requestAnimationFrame(this.animate.bind(this));
     this.render();
   }
-  
 
 /*************************************************************************/
 /******************************* LISTENERS *******************************/
@@ -44,10 +55,20 @@ class ThreeFluid {
     window.addEventListener('resize', this.onWindowResize.bind(this), false);
   }
 
+  deregisterListeners() {
+    window.removeEventListener('resize', this.onWindowResize.bind(this), false);
+  }
+
   onWindowResize() {
-    this.camera.aspect = window.innerWidth / window.innerHeight;
-    this.camera.updateProjectionMatrix();
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    // const width = this.mount.clientWidth;
+    // const height = this.mount.clientHeight;
+    // console.log('width: ' + width);
+    // console.log('height: ' + height);
+    // this.renderer.setSize(width, height, false);
+    // this.camera.aspect = width / height;
+    // this.camera.updateProjectionMatrix();
+    
+    // this.renderer.render(this.scene, this.camera);
   }
 
 /*************************************************************************/
--- app/src/three/react/ThreeCanvas.js
+++ app/src/three/react/ThreeCanvas.js
@@ -1,24 +1,24 @@
 import React, { Component } from "react";
 import ReactDOM from 'react-dom/client';
 import * as THREE from "three";
+import './three-canvas.css';
 
 import ThreeFluid from "../fluid/Fluid";
 
 class ThreeCanvas extends Component {
 
   componentDidMount() {
-    // This constructor does all the necessary initialization work
-    // for the fluid.
-    this.fluidHandle = new ThreeFluid();
-    // this.setState({fluidHandle: new ThreeFluid()});
-    // animate();
-    // this.fluidHandle.animate();
+    this.fluidHandle = new ThreeFluid(this.mount);
+  }
+
+  componentWillUnmount() {
+    this.fluidHandle.cleanup();
   }
 
   render() {
     return (
       // This tells us to call componentDidMount on this div? I don't fully understand it.
-      <div ref={ref => (this.mount = ref)} />
+      <div className="canvas" id='three-canvas' ref={(mount) => { this.mount = mount }} />
     )
   }
 
